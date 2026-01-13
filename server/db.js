f19cfc8e1e7d718927d9c119919080f81ec3a729
Sahil Hussain <140058338+saaahilhussain@users.noreply.github.com>
2026-01-13 19:48:55 +0530

    add: schema-config

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 import { MongoClient } from "mongodb";
 
-const client = new MongoClient("mongodb://127.0.0.1:27017/storageApp");
+export const client = new MongoClient("mongodb://127.0.0.1:27017/storageApp");
 
 export async function connectDB() {
   await client.connect();
