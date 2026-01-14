89cac81c09a94395ca0729b10e15eafa3deeda08
Sahil Hussain <140058338+saaahilhussain@users.noreply.github.com>
2026-01-14 23:33:04 +0530

    user registration fix

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,18 +1,13 @@
 import express from "express";
-import { writeFile } from "fs/promises";
-import directoriesData from "../directoriesDB.json" with { type: "json" };
-import usersData from "../usersDB.json" with { type: "json" };
 import checkAuth from "../middlewares/authMiddleware.js";
-import { Db } from "mongodb";
+import { ObjectId } from "mongodb";
 
 const router = express.Router();
 
 router.post("/register", async (req, res, next) => {
   const { name, email, password } = req.body;
   const db = req.db;
-
   const foundUser = await db.collection("users").findOne({ email });
-
   if (foundUser) {
     return res.status(409).json({
       error: "User already exists",
@@ -20,48 +15,42 @@ router.post("/register", async (req, res, next) => {
         "A user with this email address already exists. Please try logging in or use a different email.",
     });
   }
-
   try {
+    const rootDirId = new ObjectId();
+    const userId = new ObjectId();
     const dirCollection = db.collection("directories");
-    const usersCollection = db.collection("users");
 
-    const createdDirectory = await dirCollection.insertOne({
+    await dirCollection.insertOne({
+      _id: rootDirId,
       name: `root-${email}`,
       parentDirId: null,
+      userId,
     });
 
-    const rootDirId = createdDirectory.insertedId;
-
-    const createdUser = await usersCollection.insertOne({
+    await db.collection("users").insertOne({
+      _id: userId,
       name,
       email,
       password,
       rootDirId,
     });
 
-    await dirCollection.updateOne(
-      { _id: rootDirId },
-      { $set: { userId: createdUser.insertedId } }
-    );
-
-    return res.status(201).json({ message: "User Registered" });
+    res.status(201).json({ message: "User Registered" });
   } catch (err) {
     if (err.code === 121) {
-      return res
+      res
         .status(400)
-        .json({ error: "Invalid field, please enter valid details" });
+        .json({ error: "Invalid input, please enter valid details" });
+    } else {
+      next(err);
     }
-    next(err);
   }
 });
 
 router.post("/login", async (req, res, next) => {
   const { email, password } = req.body;
-
   const db = req.db;
-
   const user = await db.collection("users").findOne({ email, password });
-
   if (!user) {
     return res.status(404).json({ error: "Invalid Credentials" });
   }
@@ -69,7 +58,7 @@ router.post("/login", async (req, res, next) => {
     httpOnly: true,
     maxAge: 60 * 1000 * 60 * 24 * 7,
   });
-  return res.json({ message: "logged in" });
+  res.json({ message: "logged in" });
 });
 
 router.get("/", checkAuth, (req, res) => {
