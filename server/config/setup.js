5ee180de7f98f7f60656097b9fd9d579d97451d9
Sahil Hussain <140058338+saaahilhussain@users.noreply.github.com>
2026-02-05 23:21:29 +0530

    refactor: update database schema validation and improve error handling

--- server/config/setup.js
+++ server/config/setup.js
@@ -1,14 +1,18 @@
-import { bsonType } from "bson";
-import { connectDB, client } from "../db";
+import { connectDB } from "./db.js";
+import mongoose from "mongoose";
 
-const db = await connectDB();
-const command = "collMod";
+await connectDB();
+const client = mongoose.connection.getClient();
 
 try {
+  const db = mongoose.connection.db;
+  const command = "collMod";
+
   await db.command({
     [command]: "users",
     validator: {
       $jsonSchema: {
+        bsonType: "object",
         required: ["_id", "name", "email", "password", "rootDirId"],
         properties: {
           _id: {
@@ -16,10 +20,14 @@ try {
           },
           name: {
             bsonType: "string",
+            minLength: 3,
+            description:
+              "name field should a string with at least three characters",
           },
           email: {
             bsonType: "string",
-            pattern: "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$",
+            description: "please enter a valid email",
+            pattern: "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$",
           },
           password: {
             bsonType: "string",
@@ -28,15 +36,22 @@ try {
           rootDirId: {
             bsonType: "objectId",
           },
+          __v: {
+            bsonType: "int",
+          },
         },
+        additionalProperties: false,
       },
     },
+    validationAction: "error",
+    validationLevel: "strict",
   });
 
   await db.command({
     [command]: "directories",
     validator: {
       $jsonSchema: {
+        bsonType: "object",
         required: ["_id", "name", "parentDirId", "userId"],
         properties: {
           _id: {
@@ -51,16 +66,23 @@ try {
           userId: {
             bsonType: "objectId",
           },
+          __v: {
+            bsonType: "int",
+          },
         },
+        additionalProperties: false,
       },
     },
+    validationAction: "error",
+    validationLevel: "strict",
   });
 
   await db.command({
     [command]: "files",
     validator: {
       $jsonSchema: {
-        required: ["_id", "extension", "name", "parentDirId"],
+        bsonType: "object",
+        required: ["_id", "name", "extension", "userId", "parentDirId"],
         properties: {
           _id: {
             bsonType: "objectId",
@@ -69,16 +91,26 @@ try {
             bsonType: "string",
           },
           name: {
+            bsonType: "string",
+          },
+          userId: {
             bsonType: "objectId",
           },
           parentDirId: {
             bsonType: "objectId",
           },
+          __v: {
+            bsonType: "int",
+          },
         },
+        additionalProperties: false,
       },
     },
+    validationAction: "error",
+    validationLevel: "strict",
   });
 } catch (error) {
+  console.log(error);
   console.log("Error setting up database");
 } finally {
   await client.close();
